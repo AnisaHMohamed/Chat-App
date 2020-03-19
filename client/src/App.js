a8788bbded02f1373f51488bd952cc58d5f46147
Anisa <anisahamohamed@gmail.com>
2020-03-19 17:42:01 -0400

    add join and chat component with react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
-import react from 'react';
+import React from 'react';
+
 import {BrowserRouter as Router, Route}from 'react-router-dom';
-import Join from './join';
-import Chat from './chat';
+
+import Join from './components/Join';
+import Chat from './components/Chat';
 
 const App = () => (
   <Router>
