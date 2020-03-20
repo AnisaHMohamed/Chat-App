8738f05495f62785d09338a0fa58d459fd14ed24
Anisa <anisahamohamed@gmail.com>
2020-03-19 22:13:17 -0400

    user connecting and disconnecting socket and hook

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 
-import './Chat.css';
+import "./Chat.css";
 
-const Chat = () => {
-  return(
+let socket;
 
-   <h1>Chat</h1>
+const Chat = ({ location }) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const ENDPOINT = 'localhost:3001';
+
+  useEffect(() => {
+    
+    const{ name, room }= queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+    socket.emit('join', { name, room }, () => {
+      
+    });
+    return () => {
+      socket.emit('disconnect ');
+      socket.off();
+    }
+  }, [ENDPOINT, location.search]);
+  return (
+  <h1>Chat</h1>
   )
 };
 
